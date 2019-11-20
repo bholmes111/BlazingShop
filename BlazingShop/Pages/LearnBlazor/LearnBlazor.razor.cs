4c9322e9d2358bb74c75e95c2725b593f8a08e1b
bholmes111 <bholmes111@gmail.com>
2019-11-20 17:15:07 -0500

    Add project files.

---
+++ BlazingShop/Pages/LearnBlazor/LearnBlazor.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Pages.LearnBlazor
+{
+    public class LearnBlazorBase : ComponentBase
+    {
+        protected string welcomeText = "Time to learn Blazor!";
+        protected string name = "Spark";
+
+        protected void getName()
+        {
+            name = "EventName";
+        }
+    }
+}
---
+++ BlazingShop/Services/CategoryService.cs
@@ -0,0 +1,68 @@
+﻿using BlazingShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Services
+{
+    public class CategoryService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Category GetCategory(int categoryId)
+        {
+            Category obj = new Category();
+            return _db.Categories.FirstOrDefault(c => c.Id == categoryId);
+        }
+
+        public List<Category> GetCategories()
+        {
+            return _db.Categories.ToList();
+        }
+        public bool CreateCategory(Category objCategory)
+        {
+            _db.Categories.Add(objCategory);
+            _db.SaveChanges();
+            return true;
+        }
+        public bool UpdateCategory(Category objCategory)
+        {
+            Category existingCategory = _db.Categories.FirstOrDefault(c => c.Id == objCategory.Id);
+
+            if(existingCategory != null)
+            {
+                existingCategory.Name = objCategory.Name;
+                _db.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool DeleteCategory(Category objCategory)
+        {
+            Category existingCategory = _db.Categories.FirstOrDefault(c => c.Id == objCategory.Id);
+
+            if (existingCategory != null)
+            {
+                _db.Categories.Remove(objCategory);
+                _db.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
